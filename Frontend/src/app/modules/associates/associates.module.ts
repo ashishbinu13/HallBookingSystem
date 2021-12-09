bad31b1d3dd739ab616f0f47e5f9c3406bd17f98
Ashish <ashish.binu1@gmail.com>
2021-12-09 21:20:24 +0530

    directory structure

---
+++ Frontend/src/app/modules/associates/associates.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AssociatesRoutingModule } from './associates-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AssociatesRoutingModule
+  ]
+})
+export class AssociatesModule { }
