b51120cf33af36ab69d45cf9e7a42f752f0fbb45
Ashish <ashish.binu1@gmail.com>
2021-12-10 12:34:40 +0530

    user model created

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const { role } = require("../helpers/user.role");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  userId: {
+    type: String,
+    lowercase: true,
+    default: "ICTAK-ID",
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: [role.admin, role.associate],
+    default: role.associate,
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+UserSchema.methods.isValidPassword = async function (password) {
+  try {
+    return await bcrypt.compare(password, this.password);
+  } catch (error) {
+    throw error;
+  }
+};
+
+const User = mongoose.model("user", UserSchema);
+module.exports = User;
