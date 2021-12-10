2b9c58cf91c8154388dd1a024c8318e1ce4b4ae2
seethalthom <annmariyajhn@gmail.com>
2021-12-11 00:12:15 +0530

    login page

--- Backend/app.js
+++ Backend/app.js
@@ -1,21 +1,21 @@
 const express = require("express");
 const createHttpError = require("http-errors");
-require("dotevn").config();
+require("dotenv").config();
 
 const app = express();
 
-app.use(async (req, res, next) => {
-  next(createHttpError.NotFound());
+app.use(async(req, res, next) => {
+    next(createHttpError.NotFound());
 });
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.send({
-    error: err.status || 500,
-    message: err.message,
-  });
+    res.status(err.status || 500);
+    res.send({
+        error: err.status || 500,
+        message: err.message,
+    });
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`server started on PORT: ${PORT}`);
+    console.log(`server started on PORT: ${PORT}`);
 });
