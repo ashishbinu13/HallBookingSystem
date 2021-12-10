bb8a834869720e8959ed9a42bc7022ca86bae183
seethalthom <annmariyajhn@gmail.com>
2021-12-11 03:42:06 +0530

    addbooking

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
+import { AddBookingComponent } from './modules/admin/components/add-booking/add-booking.component';
 
 const routes: Routes = [{path:'', component:LoginComponent},
-{path:'home', component:DashboardComponent}];
+{path:'home', component:DashboardComponent},
+{path:'addbooking', component:AddBookingComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
