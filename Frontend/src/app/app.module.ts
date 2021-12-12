8200d40395e3fef2ce52849f82aae92ad898f0ef
Ashish <ashish.binu1@gmail.com>
2021-12-12 16:45:04 +0530

    calendar component created

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,27 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
+// fullcalendar
+import { FullCalendarModule } from '@fullcalendar/angular';
+import interactionPlugin from '@fullcalendar/interaction';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
-
-
+FullCalendarModule.registerPlugins([
+  timeGridPlugin,
+  interactionPlugin,
+  dayGridPlugin,
+]);
+//
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddBookingComponent } from './modules/admin/components/add-booking/add-booking.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AddBookingComponent
-  ],
+  declarations: [AppComponent, AddBookingComponent, CalendarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
-
+    FormsModule,
+    FullCalendarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
